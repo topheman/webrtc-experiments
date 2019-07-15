671725393e15a31a0661d81ab4f3c10d87c25012
Christophe Rosset <tophe@topheman.com>
2019-07-15 20:37:12 +0200

    feat(peerjs): send disconnect to peers on unload

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,22 @@ import { makeStore } from "./logic.js";
 
 function makePeerMaster(store) {
   const peer = new Peer(getMasterPeerIdFromLocalStorage());
+  const connections = [];
+  // send a disconnect message to all clients when reloading/closing
+  window.addEventListener("beforeunload", () => {
+    console.log("disconnecting", connections);
+    connections.forEach(conn => {
+      console.log(`Sending "DISCONNECT" to ${conn.peer}`);
+      conn.send({ type: "DISCONNECT" });
+    });
+  });
   peer.on("open", peerId => {
     setMasterPeerIdToLocalStorage(peerId);
     console.log("Peer object created", { peerId });
     makeQRCode(makePeerUrl(peerId));
   });
   peer.on("connection", conn => {
+    connections.push(conn);
     console.log(`Data connection opened with ${conn.peer}`, conn);
     conn.on("data", data => {
       console.log("Incomming data", data);
@@ -21,6 +31,8 @@ function makePeerMaster(store) {
       console.log("counter", store.getState());
     });
   });
+  peer.on("error", error => console.error(error));
+  peer.on("disconnected", e => console.log("disconnected", e));
   return peer;
 }
 
--- src/js/remote.js
+++ src/js/remote.js
@@ -2,25 +2,43 @@ import { getPeerIdFromLacationHash } from "./common.js";
 
 function makePeerRemote(masterPeerId) {
   const peer = new Peer();
+  let connOpen = false;
   peer.on("open", peerId => {
     console.log("Peer object created", { peerId });
     const conn = peer.connect(masterPeerId);
     conn.on("open", () => {
+      connOpen = true;
       document
         .querySelector(".counter-control-add")
-        .addEventListener("click", e => {
-          console.log("ADD");
-          conn.send({ type: "COUNTER_ADD", id: peerId });
+        .addEventListener("click", () => {
+          if (connOpen) {
+            console.log("COUNTER_ADD");
+            conn.send({ type: "COUNTER_ADD", id: peerId });
+          } else {
+            console.warn("COUNTER_ADD not sent - connection closed");
+          }
         });
       document
         .querySelector(".counter-control-sub")
-        .addEventListener("click", e => {
-          console.log("SUB");
-          conn.send({ type: "COUNTER_SUB", id: peerId });
+        .addEventListener("click", () => {
+          if (connOpen) {
+            console.log("SUB");
+            conn.send({ type: "COUNTER_SUB", id: peerId });
+          } else {
+            console.warn("COUNTER_SUB not sent - connection closed");
+          }
         });
       console.log(`Data connection opened with ${masterPeerId}`, conn);
     });
+    conn.on("data", data => {
+      console.log("Incomming data", data);
+      if (data && data.type === "DISCONNECT") {
+        connOpen = false;
+      }
+    });
   });
+  peer.on("error", error => console.error(error));
+  peer.on("disconnected", e => console.log("disconnected", e));
   return peer;
 }
 
