f05434764abac5fd503437ee33aca26a02fb16a3
Christophe Rosset <tophe@topheman.com>
2019-07-22 20:03:54 +0200

    feat(redux): add applyMiddleware to redux-lite

--- src/js/redux-lite.js
+++ src/js/redux-lite.js
@@ -29,3 +29,54 @@ export function createStore(initialReducer, initialState = {}, enhancer) {
     }
   };
 }
+
+/**
+ * from redux source code - https://github.com/reduxjs/redux/blob/master/src/applyMiddleware.js
+ */
+export function applyMiddleware(...middlewares) {
+  return createStore => (...args) => {
+    const store = createStore(...args);
+    let dispatch = () => {
+      throw new Error(
+        "Dispatching while constructing your middleware is not allowed. " +
+          "Other middleware would not be applied to this dispatch."
+      );
+    };
+
+    const middlewareAPI = {
+      getState: store.getState,
+      dispatch: (...args) => dispatch(...args)
+    };
+    const chain = middlewares.map(middleware => middleware(middlewareAPI));
+    dispatch = compose(...chain)(store.dispatch);
+
+    return {
+      ...store,
+      dispatch
+    };
+  };
+}
+
+/**
+ * from redux source code - https://github.com/reduxjs/redux/blob/master/src/compose.js
+ *
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for
+ * the resulting composite function.
+ *
+ * @param {...Function} funcs The functions to compose.
+ * @returns {Function} A function obtained by composing the argument functions
+ * from right to left. For example, compose(f, g, h) is identical to doing
+ * (...args) => f(g(h(...args))).
+ */
+export function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)));
+}
