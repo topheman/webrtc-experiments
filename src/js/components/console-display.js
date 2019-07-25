d7f97d2454b17704cda666784ee2b55e834d871c
Christophe Rosset <tophe@topheman.com>
2019-07-25 08:44:22 +0200

    feat(*): add collapse on logs

--- src/js/components/console-display.js
+++ src/js/components/console-display.js
@@ -4,16 +4,53 @@ class ConsoleDisplay extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const style = document.createElement("style");
     const ul = document.createElement("ul");
+    const div = document.createElement("div");
+    const header = document.createElement("span");
+    header.textContent = "Logs";
+    header.className = "header";
+    header.addEventListener(
+      "click",
+      () => {
+        const rootDivClassList = this.shadowRoot.querySelector("div").classList;
+        rootDivClassList.toggle("slidedown");
+        rootDivClassList.toggle("slideup");
+      },
+      false
+    );
+    div.className = "slidedown";
+    div.appendChild(header);
+    div.appendChild(ul);
     style.textContent = `
+:host {
+  --border-color: grey;
+}
+.header {
+  padding: 5px;
+  border: 1px solid var(--border-color);
+  border-bottom: 0px;
+  display: block;
+  cursor: pointer;
+}
+.slideup .header:after {
+  content: " 🔽";
+}
+.slidedown .header:after {
+  content: " 🔼";
+}
 ul {
   list-style: none;
   padding-left: 0;
+  margin-top: 0;
   width: 100%;
   overflow-x: scroll;
+  border-bottom: 1px solid var(--border-color);
 }
 li {
   padding-left: 5px;
   white-space: nowrap;
+  border: 1px solid var(--border-color);
+  border-bottom: 0px;
+  border-top: 0px;
 }
 li.two {
   background: #f1f1f1;
@@ -33,9 +70,20 @@ li.warn::before {
 li.log::before {
   content: "📋"
 }
+.slideup ul, .slidedown ul {
+    max-height: 0;            
+    overflow-y: hidden;
+    -webkit-transition: max-height 0.5s ease-in-out;
+    -moz-transition: max-height 0.5s ease-in-out;
+    -o-transition: max-height 0.5s ease-in-out;
+    transition: max-height 0.5s ease-in-out;
+}
+.slidedown ul {            
+    max-height: 1000px ;                    
+}
     `;
     shadow.appendChild(style);
-    shadow.appendChild(ul);
+    shadow.appendChild(div);
     this.render();
   }
 
