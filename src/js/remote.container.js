38967522f61216d20f3fba5a65adf89e01f33d30
Christophe Rosset <tophe@topheman.com>
2019-08-08 16:37:23 +0200

    feat(*): style for disabled buttons

--- src/js/remote.container.js
+++ src/js/remote.container.js
@@ -11,6 +11,7 @@ export function createView(templateNode, staticContent, store, events) {
   const loader = content.querySelector(".initial-loading");
   const errorsDisplay = content.querySelector("errors-display");
   const formInput = content.querySelector(".form-set-name input");
+  const formButton = content.querySelector(".form-set-name button");
   const buttons = content.querySelectorAll(".counter-control button");
   formInput.value = getRemoteNameFromSessionStorage();
   const consoleDisplay = content.querySelector("console-display");
@@ -46,18 +47,13 @@ export function createView(templateNode, staticContent, store, events) {
     if (state.common.peerId || state.common.signalErrors.length > 0) {
       loader.classList.add("hide");
     }
-    buttons.forEach(button => {
+    [...buttons, formInput, formButton].forEach(button => {
       if (isDisconnected(state)) {
         button.setAttribute("disabled", "");
       } else {
         button.removeAttribute("disabled");
       }
     });
-    if (isDisconnected(state)) {
-      formInput.setAttribute("disabled", "");
-    } else {
-      formInput.removeAttribute("disabled");
-    }
     errorsDisplay.data = [
       isDisconnected(state) && isLocalIp(location.hostname)
         ? `You're disconnected.<br/>You are running the app in development on a <strong>local ip</strong> (${location.hostname}) (<strong>without https</strong>) which leads to disconnections.<br/><strong>Use tools such as localtunnel.me or ngrock.io to test with mobile devices</strong>.<br/>Features such as <strong>WebRTC need https</strong> in production.`
